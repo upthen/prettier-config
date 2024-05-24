fe5d64c0995f2406a41a6bdc12256f91d6079d1a
yongbo.zeng_U+ <yongbo.zeng@united-imaging.com>
2024-05-24 23:33:12 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,13 @@
+exports.default = {
+	"arrowParens": "always",
+	"printWidth": 80,
+	"tabWidth": 2,
+	"useTabs": true,
+	"semi": false,
+	"singleQuote": true,
+	"trailingComma": "all",
+	"bracketSpacing": true,
+	"jsxSingleQuote": false,
+	"quoteProps": "as-needed",
+	"bracketSameLine": false
+}
